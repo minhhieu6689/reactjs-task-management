6b0026ce0d35ece0c9fb98a9c9f154473a2058a2
minhhieu6689 <minhhieusw2279@gmail.com>
2020-06-08 20:21:38 +0700

    Update App.js

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -22,6 +22,7 @@ class App extends Component {
         var result = null ;
         if (routes.length > 0)
         {
+            console.log(123)
             result = routes.map((route, index) => {
                 return (
                     <Route 
